7d60829598c261a70d1f9dbb31a4d05d1c7c9798
pelikhan <jhalleux@microsoft.com>
2023-06-07 14:18:14 -0700

    rewrite test

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
-import { pins, board } from "@dsboard/adafruit_qt_py_c3"
-import { fetch } from "@devicescript/net"
-import { schedule } from "@devicescript/runtime"
-import { readSetting } from "@devicescript/settings"
-import { startSHTC3 } from "@devicescript/drivers"
+import { describe, test } from "@devicescript/test"
 import { createData } from "."
 
-const { temperature } = await startSHTC3()
-schedule(
-    async () => {
-        const value = await temperature.reading.read()
-        const status = await createData(value)
-        console.log({ status })
-    },
-    { timeout: 1000, interval: 10000 }
-)
+describe("createData", () => {
+    test("0", async () => {
+      //  await createData(0)
+    })
+})
