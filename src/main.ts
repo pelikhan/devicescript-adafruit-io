bef19517ab841a1d3d841271f542d0cc7e3ad328
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-07-27 15:14:06 +0000

    add interval in sample

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { delay } from "@devicescript/core"
 import { createData, publishData, startAdafruitIOMQTTClient } from "."
+import { schedule } from "@devicescript/runtime"
 
 console.log(`post data using http`)
 await createData(123)
@@ -7,4 +8,10 @@ await createData(123)
 console.log(`post data using mqtt in 5s`)
 await delay(5000)
 const client = await startAdafruitIOMQTTClient()
-await publishData(client, 456)
+
+schedule(
+    async () => {
+        await publishData(client, 456)
+    },
+    { interval: 5000 }
+)
